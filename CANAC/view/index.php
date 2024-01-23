b2329ed22f1e9611b5441f35b7c43cbd48bab267
CristhianUri <113977591+CristhianUri@users.noreply.github.com>
2024-01-23 02:48:45 -0600

    cambios subidos
    se realizaron cambios debido a problemas de responsividad

---
+++ CANAC/view/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+        integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="estilo.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <title>CANACINTRA TEHUACÁN </title>
+</head>
+
+<body>
+    <!--SECCIÓN INICIO-->
+
+    <div class="contianer">
+        <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #0C061D;">
+            <div class="container-fluid">
+                <a class="navbar-brand ms-5" href="#">Bachillerato digital No. 43</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="contenido navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link mx-1" aria-current="page" href="#inicio">INICIO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-1" href="#org">CONOCENOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-1" href="#">SERVICIOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-1" href="#">CONVENIOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link  mx-1" href="#">NOTICIAS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-1" href="#">CONTACTANOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-1" href="#">INICIAR SESION</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    </div>
+   
+            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <img height="800px" width="1800px" src="/img/1.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img height="800px" src="/img/2.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img height="800px" src="/img/3.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+ 
+
+
+    <div class="container mt-4" id="#org">
+        <div class="row justify-content-center">
+            <div class="text-center">
+                <h3>SOBRE NOSOTROS</h3>
+            </div>
+            <div class="col-sm-4  my-2 ">
+                <div class="card">
+                    <img class="img-thumbnail"
+                        src="https://2.bp.blogspot.com/-df2MxLmx1Bs/T_ETvZo2iWI/AAAAAAAAACA/WKwnXOaogRA/s1600/BD.jpg"
+                        class="card-img-top" alt="...">
+                    <h5 class="text-center mt-2 text-dark ">
+                        MISIÓN
+                    </h5>
+                    <div class="card-body" style="height: 13rem;">
+                        <p class="card-text  text-center text-dark">Servir a la comunidad formando
+                            alumnos con habilidades y competencias que les permitan desarrollarse
+                            profesionalmente para enfrentar los retos y desafíos del entorno local,
+                            regional y nacional; orientado por profesores íntegros y de calidad en la
+                            enseñanza.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 my-2">
+                <div class="card ">
+                    <img class="img-thumbnail"
+                        src="https://2.bp.blogspot.com/-df2MxLmx1Bs/T_ETvZo2iWI/AAAAAAAAACA/WKwnXOaogRA/s1600/BD.jpg"
+                        alt="...">
+                    <h5 class="text-center text-center mt-2 text-dark">
+                        VISIÓN
+                    </h5>
+                    <div class="card-body" style="height: 13rem;">
+
+                        <p class="card-text text-center text-dark">
+                            Posicionarse como centro educativo
+                            de referencia y ser un instrumento de colaboración, participación y desarrollo
+                            que facilite un proceso de enseñanza-aprendizaje de excelencia, creativa e innovadora
+                            que permita continuar con estudios superiores y/o integrarse al ámbito laboral.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 my-2">
+                <div class="card">
+                    <img class="img-thumbnail"
+                        src="https://2.bp.blogspot.com/-df2MxLmx1Bs/T_ETvZo2iWI/AAAAAAAAACA/WKwnXOaogRA/s1600/BD.jpg"
+                        class="card-img-top" alt="...">
+                    <h5 class="text-center mt-2 text-dark ">
+                        VALORES
+                    </h5>
+                    <div class="card-body" style="height: 13rem;">
+
+                        <p class="card-text text-center text-dark">
+                            Responsabilidad, Respeto, Armonía,
+                            Amistad, Honestidad, Lealtad, Cooperación.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</body>
+
+</html>
